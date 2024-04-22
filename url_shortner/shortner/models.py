97cc985bae7e26d27c938faa9fd874d045c6c23e
syedjaferk <learnwithjafer@gmail.com>
2024-04-22 21:36:11 +0530

    implement basic url shortener

--- url_shortner/shortner/models.py
+++ url_shortner/shortner/models.py
@@ -2,6 +2,11 @@
 Models for ShortUrls
 """
 import uuid
+import pytz
+import string
+import random
+from datetime import datetime, timedelta
+from django.utils import timezone
 from django.db import models
 
 
@@ -10,6 +15,20 @@ class ShortUrls(models.Model):
     Fields required for short urls
     """
     url = models.URLField(max_length=300)
-    referenceId = models.UUIDField(default=uuid.uuid4, editable=False)
+    referenceId = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.url} | {self.created_at}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.referenceId = self.generate_short_code()
+            self.expires_at = timezone.now() + timedelta(days=30)
+        return super().save(*args, **kwargs)
+
+    def generate_short_code(self):
+        characters = string.ascii_letters + string.digits
+        short_code = ''.join(random.choice(characters) for _ in range(6))
+        return short_code
--- url_shortner/shortner/views.py
+++ url_shortner/shortner/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from shortner.models import ShortUrls
 
 
 class GetShortenUrl(View):
@@ -10,11 +11,12 @@ class GetShortenUrl(View):
     View class to get the actual url given shortened url
     """
     @csrf_exempt
-    def get(self, request, referenceId):
+    def get(self, _, referenceId):
         """
         Get method to handle the redirection of the shortened url to the actual url
         """
-        return JsonResponse({'result': referenceId})
+        url_object = ShortUrls.objects.filter(referenceId=referenceId)[0]
+        return JsonResponse({'url': url_object.url})
 
 class CreateShortUrl(View):
     """
@@ -27,5 +29,7 @@ class CreateShortUrl(View):
         """
         request_data = json.loads(request.body)
         url = request_data.get("url")
-        return JsonResponse({'result': 'success', 'url': url})
-
+        short_url_obj = ShortUrls(url=url)
+        short_url_obj.save()
+        short_url = request.build_absolute_uri('/') + short_url_obj.referenceId
+        return JsonResponse({'result': 'success', 'url': short_url})
